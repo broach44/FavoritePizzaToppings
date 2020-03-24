b3b758a05a6ad1c7fcaca8ae0f2242bfc4eb1cf8
Crystal Broach <crysland@gmail.com>
2020-03-23 21:25:13 -0500

    Listing Toppings from Json file

--- FavoritePizzaToppings/Program.cs
+++ FavoritePizzaToppings/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace FavoritePizzaToppings
 {
@@ -6,7 +10,33 @@ namespace FavoritePizzaToppings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var pizzas = JsonConvert.DeserializeObject<List<Pizza>>(File.ReadAllText(@"./pizzas.json"));
+
+            var toppingLists = pizzas.Select(p => string.Join(",", p.Toppings.OrderBy(t => t)));
+            
+            //var distinctToppingCombinations = toppingLists.Distinct();
+
+            var countOfCombination = new Dictionary<string, int>();
+
+            foreach (var combination in toppingLists)
+            {
+                if (!countOfCombination.ContainsKey(combination))
+                {
+                    countOfCombination.Add(combination, 1);
+                }
+                else
+                {
+                    countOfCombination[combination] += 1;
+                }
+            }
+
+            var mostOrdered = countOfCombination.OrderByDescending(item => item.Value).Take(20);
+
+            foreach (var (combination, count) in mostOrdered)
+            {
+                Console.WriteLine($"The topping combination of {combination} was ordered {count} times.");
+
+            }
         }
     }
 }
